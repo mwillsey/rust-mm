53440ed6106e9bca1e4b98e25e51c488ebff5552
Max Willsey <me@mwillsey.com>
2020-11-14 11:22:29 -0800

    Parallel

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,68 @@
-#![allow(non_snake_case)]
-
-#[inline(always)]
-unsafe fn get<T: Copy>(p: *const T, offset: usize) -> T {
-    p.offset(offset as isize).read()
+#[derive(Clone, Copy)]
+struct Matrix {
+    ptr: *const f32,
+    cols: usize,
 }
 
-#[inline(always)]
-unsafe fn get_mut<T: Copy>(p: *mut T, offset: usize) -> *mut T {
-    p.offset(offset as isize)
+impl Matrix {
+    #[inline(always)]
+    unsafe fn get(self, y: usize, x: usize) -> f32 {
+        let offset = y * self.cols + x;
+        self.ptr.offset(offset as isize).read()
+    }
 }
+
+#[derive(Clone, Copy)]
+struct MutMatrix {
+    ptr: *mut f32,
+    cols: usize,
+}
+
+impl MutMatrix {
+    #[inline(always)]
+    unsafe fn get_mut(self, y: usize, x: usize) -> *mut f32 {
+        let offset = y * self.cols + x;
+        self.ptr.offset(offset as isize)
+    }
+}
+
+unsafe impl Send for Matrix {}
+unsafe impl Send for MutMatrix {}
+unsafe impl Sync for Matrix {}
+unsafe impl Sync for MutMatrix {}
 
 pub unsafe fn sgemm(m: usize, k: usize, n: usize, a: *const f32, b: *const f32, c: *mut f32) {
-    let (M, K, N) = (m, k, n);
-    let A = |y: usize, x: usize| get(a, y * K + x);
-    let B = |y: usize, x: usize| get(b, y * N + x);
-    let C = |y: usize, x: usize| get_mut(c, y * N + x);
-    for i in 0..M {
-        for k in 0..K {
-            for j in 0..N {
-                *C(i, j) += A(i, k) * B(k, j);
+    let a = Matrix { ptr: a, cols: k };
+    let b = Matrix { ptr: b, cols: n };
+    let c = MutMatrix { ptr: c, cols: n };
+
+    use rayon::prelude::*;
+
+    (0..m).into_par_iter().for_each(move |i| {
+        for k in 0..k {
+            for j in 0..n {
+                *c.get_mut(i, j) += a.get(i, k) * b.get(k, j);
             }
         }
-    }
+    });
+
+    // let block_size = 16;
+    // let block = |block_n: usize| {
+    //     let start = block_n * block_size;
+    //     start..(start + block_size)
+    // };
+
+    // (0..m).into_par_iter().for_each(move |ii| {
+    //         for bj in 0..(n / block_size) {
+    //     for bk in 0..(k / block_size) {
+    //             for kk in block(bk) {
+    //                 for jj in block(bj) {
+    //                     *c.get_mut(ii, jj) += a.get(ii, kk) * b.get(kk, jj);
+    //                 }
+    //             }
+    //         }
+    //     }
+    // });
 }
 
 #[cfg(test)]
@@ -52,7 +93,7 @@ mod tests {
         let rsc = n as isize;
         let csc = 1;
         #[rustfmt::skip]
-        matrixmultiply::sgemm(m, k, n, alpha,
+        matrixmultiply_mt::sgemm(m, k, n, alpha,
                               a, rsa, csa,
                               b, rsb, csb,
                               beta, c, rsc, csc);
@@ -67,7 +108,7 @@ mod tests {
         }
     }
 
-    fn bench_one(iters: usize, name: &str, mut f: impl FnMut()) {
+    fn bench_one(iters: usize, flops: f64, name: &str, mut f: impl FnMut()) {
         let mut ts = vec![];
         for _ in 0..iters {
             let t = Instant::now();
@@ -77,9 +118,10 @@ mod tests {
         let sum: Duration = ts.iter().sum();
         let avg = sum / iters as u32;
         let min = ts.iter().min().unwrap();
+        let avg_gflops = (flops / 1e9) / avg.as_secs_f64();
         println!(
-            "{} iters of {} in {:?}, avg: {:?}, min: {:?}",
-            iters, name, sum, avg, min
+            "{} iters of {} in {:?}, avg: {:?}, min: {:?}, avg GFLOPS: {}",
+            iters, name, sum, avg, min, avg_gflops
         );
     }
 
@@ -91,15 +133,16 @@ mod tests {
         println!("Benching {} iters of {}x{}", iters, n, n);
 
         let (m, k, n) = (n, n, n);
+        let flops = (2 * m * n * k) as f64;
         let a = mat(m, k, |i, j| i + j);
         let b = mat(k, n, |i, j| i + j);
         let mut c1 = mat(m, n, |_, _| 0);
         let mut c2 = mat(m, n, |_, _| 0);
 
-        bench_one(iters, "sgemm", || unsafe {
+        bench_one(iters, flops, "sgemm", || unsafe {
             sgemm(m, k, n, a.as_ptr(), b.as_ptr(), c1.as_mut_ptr());
         });
-        bench_one(iters, "mm_sgemm", || unsafe {
+        bench_one(iters, flops, "mm_sgemm", || unsafe {
             mm_sgemm(m, k, n, a.as_ptr(), b.as_ptr(), c2.as_mut_ptr());
         });
     }
